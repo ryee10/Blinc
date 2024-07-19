baca0549195dbc60294585a5a4ad49e4d3d1ff01
rye <ryckiel.malaca@gmail.com>
2024-07-19 09:45:03 +0800

    eyy

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+
+const Header = () => {
+  return (
+    <View style={[styles.container]}>
+        <TouchableOpacity style={[styles.roundBtn]}>
+            <Text style={[]}>
+            JD
+            </Text>
+        </TouchableOpacity>
+      
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        gap: 10,
+
+    },
+    roundBtn: {
+        width: 40,
+        height: 40,
+        borderRadius:20,
+        backgroundColor: '#cccccc'
+    }
+})
+export default Header
